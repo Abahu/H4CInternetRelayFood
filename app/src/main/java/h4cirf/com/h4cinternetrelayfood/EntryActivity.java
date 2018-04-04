b7770a7bb54255c73b626f8af16025bdc9963fde
Abahu <diem.carpe.rs@gmail.com>
2018-04-03 21:51:02 -0700

    Check the comment in EntryActivity

--- app/src/main/java/h4cirf/com/h4cinternetrelayfood/EntryActivity.java
+++ app/src/main/java/h4cirf/com/h4cinternetrelayfood/EntryActivity.java
@@ -3,6 +3,16 @@ package h4cirf.com.h4cinternetrelayfood;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/*
+ * Comment your name here, save, commit, and then push to show us all you have this working.
+ * Abahu
+ */
+
+/**
+ * Created by Abahu
+ * The entry activity from which we can load all of our assets/navigate to the more functional
+ * parts of the app
+ */
 public class EntryActivity extends AppCompatActivity {
 
     @Override
