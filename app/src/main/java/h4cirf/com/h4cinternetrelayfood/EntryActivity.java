ff5022bff59a92a221d2ca0f755b4138af8e9b9c
catherinedw <catwhitaker@gmail.com>
2018-04-07 10:30:51 -0700

    HttpGetRequest class

--- app/src/main/java/h4cirf/com/h4cinternetrelayfood/EntryActivity.java
+++ app/src/main/java/h4cirf/com/h4cinternetrelayfood/EntryActivity.java
@@ -8,6 +8,8 @@ import com.amazonaws.mobile.client.AWSMobileClient;
 import com.amazonaws.mobile.client.AWSStartupHandler;
 import com.amazonaws.mobile.client.AWSStartupResult;
 
+import java.util.concurrent.ExecutionException;
+
 /*
  * Comment your name here, save, commit, and then push to show us all you have this working.
  * Abahu
@@ -20,6 +22,7 @@ import com.amazonaws.mobile.client.AWSStartupResult;
  */
 public class EntryActivity extends AppCompatActivity {
     // TESTING :D
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,5 +33,15 @@ public class EntryActivity extends AppCompatActivity {
                 Log.d("YourMainActivity", "AWSMobileClient is instantiated and you are connected to AWS!");
             }
         }).execute();
+    }
+    public void myMethod() throws ExecutionException, InterruptedException{
+        //Some url endpoint that you may have
+        String myUrl = "http://www.google.com";
+        //String to place our result in
+        String result;
+        //Instantiate new instance of our class
+        HttpGetRequest getRequest = new HttpGetRequest();
+        //Perform the doInBackground method, passing in our url
+        result = getRequest.execute(myUrl).get();
     }
 }
